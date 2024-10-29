526c8ecb9b54f299aa1922de3f2088b00eeb1d3d
Qynn Schwaab <qynn@commonsense.dev>
2024-10-28 21:44:40 -0400

    chore: allow running tests if env. vars missing or token expired

--- tests/test_verify_token.py
+++ tests/test_verify_token.py
@@ -1,12 +1,19 @@
 import jwt
 import pytest
+from warnings import warn
+from .conftest import has_env_vars
 from clerk_backend_api.jwks_helpers import (
     verify_token,
     TokenVerificationErrorReason,
     TokenVerificationError,
+    VerifyTokenOptions,
 )
 
 
+@pytest.mark.skipif(
+    not has_env_vars(['CLERK_SECRET_KEY']),
+    reason="CLERK_SECRET_KEY environment variable must be set"
+)
 def test_verify_token_invalid_token(vt_options):
 
     vt_options.jwt_key = None
@@ -16,6 +23,10 @@ def test_verify_token_invalid_token(vt_options):
     assert exc_info.value.reason == TokenVerificationErrorReason.TOKEN_INVALID
 
 
+@pytest.mark.skipif(
+    not has_env_vars(['CLERK_SECRET_KEY']),
+    reason="CLERK_SECRET_KEY environment variable must be set"
+)
 def test_verify_token_public_key_invalid_kid(vt_options):
 
     vt_options.jwt_key = None
@@ -26,6 +37,10 @@ def test_verify_token_public_key_invalid_kid(vt_options):
     assert exc_info.value.reason == TokenVerificationErrorReason.JWK_KID_MISMATCH
 
 
+@pytest.mark.skipif(
+    not has_env_vars(['CLERK_SESSION_TOKEN']),
+    reason="CLERK_SESSION_TOKEN environment variable must be set"
+)
 def test_verify_token_missing_secret_key(session_token, vt_options):
 
     vt_options.jwt_key = None
@@ -36,6 +51,10 @@ def test_verify_token_missing_secret_key(session_token, vt_options):
     assert exc_info.value.reason == TokenVerificationErrorReason.SECRET_KEY_MISSING
 
 
+@pytest.mark.skipif(
+    not has_env_vars(['CLERK_SESSION_TOKEN']),
+    reason="CLERK_SESSION_TOKEN environment variable must be set"
+)
 def test_verify_token_invalid_secret_key(session_token, vt_options):
 
     vt_options.jwt_key = None
@@ -46,6 +65,10 @@ def test_verify_token_invalid_secret_key(session_token, vt_options):
     assert exc_info.value.reason == TokenVerificationErrorReason.JWK_FAILED_TO_LOAD
 
 
+@pytest.mark.skipif(
+    not has_env_vars(['CLERK_SESSION_TOKEN']),
+    reason="CLERK_SESSION_TOKEN environment variable must be set"
+)
 def test_verify_token_invalid_jwt_key(session_token, vt_options):
 
     vt_options.jwt_key = 'invalid_pem_key'
@@ -56,18 +79,41 @@ def test_verify_token_invalid_jwt_key(session_token, vt_options):
     assert exc_info.value.reason == TokenVerificationErrorReason.JWK_FAILED_TO_RESOLVE
 
 
+def assert_payload(session_token: str, vt_options: VerifyTokenOptions):
+    payload = {}
+    expired = False
+
+    try:
+        payload = verify_token(session_token, vt_options)
+    except TokenVerificationError as e:
+        if e.reason != TokenVerificationErrorReason.TOKEN_EXPIRED:
+            raise
+        expired = True
+        warn("the provided session token is expired.")
+
+    if expired:
+        assert payload == {}
+    else:
+        assert payload.get('azp') in vt_options.authorized_parties
+
+
+@pytest.mark.skipif(
+    not has_env_vars(['CLERK_SECRET_KEY', 'CLERK_SESSION_TOKEN']),
+    reason="CLERK_SECRET_KEY and CLERK_SESSION_TOKEN environment variables must be set"
+)
 def test_verify_token_remote_ok(session_token, vt_options):
 
     vt_options.jwt_key = None
-    payload = verify_token(session_token, vt_options)
-    assert payload is not None
-    assert payload.get('azp') in vt_options.authorized_parties
+
+    assert_payload(session_token, vt_options)
 
 
+@pytest.mark.skipif(
+    not has_env_vars(['CLERK_JWT_KEY', 'CLERK_SESSION_TOKEN']),
+    reason="CLERK_JWT_KEY and CLERK_SESSION_TOKEN environment variables must be set"
+)
 def test_verify_token_local_ok(session_token, vt_options):
 
     assert vt_options.jwt_key is not None
 
-    payload = verify_token(session_token, vt_options)
-    assert payload is not None
-    assert payload.get('azp') in vt_options.authorized_parties
+    assert_payload(session_token, vt_options)
